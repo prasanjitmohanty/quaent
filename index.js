40e6aa1a6acd0c3763fbff3c3b03fffc678806f7
PrasanjitMohanty <prasanjit.mohanty@immediate.co.uk>
2017-07-03 10:17:46 +0100

    removing share count npm package

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const searchHelper = require('./search-helper');
-const shares = require('shares');
 
 const searchText = 'What are the upcoming trends on chatbots?';
 searchHelper.getArticleLinks(searchText).then(links => {
