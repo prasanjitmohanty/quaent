ce179dced3434c92cb35dbea4dac5edc54d3583a
PrasanjitMohanty <prasanjit.mohanty@immediate.co.uk>
2017-07-19 14:52:16 +0100

    UI changes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule }    from '@angular/http';
+import {MdCardModule,MdGridListModule,MdSidenavContainer,MdToolbar} from '@angular/material';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MdSidenavContainer
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    MdCardModule,
+    NoopAnimationsModule,
+    MdGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
