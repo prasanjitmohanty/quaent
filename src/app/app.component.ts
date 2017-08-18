55ebc1e552e2c8a75fad1ea4d4e393c75fc4d773
PrasanjitMohanty <prasanjit.mohanty@immediate.co.uk>
2017-08-18 13:51:21 +0100

    Adding progressbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchService } from './search.service';
 import { Subject } from 'rxjs/Subject';
-import {Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -10,25 +10,37 @@ import {Subscription} from 'rxjs';
   styleUrls: ['./app.component.css'],
   providers: [SearchService]
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   results: Object;
-  title ='Search quality content from the web...';
+  title = 'Search quality content from the web...';
   busy: Subscription;
 
   ngOnInit() {
-       // this.busy = this.http.get('...').subscribe();
-    }
+    // this.busy = this.http.get('...').subscribe();
+  }
 
   constructor(private searchService: SearchService) { }
 
-  performSearch(searchTerm: string){
-    if(searchTerm){
-    console.log("searchTerm::",searchTerm);
-    this.busy = this.searchService.searchEntries(searchTerm)
-      .subscribe(results => {
-        console.log(results);
-        this.results = results;
-      });
+  performSearch(searchTerm: string) {
+    if (searchTerm) {
+      console.log("searchTerm::", searchTerm);
+      this.busy = this.searchService.searchEntries(searchTerm)
+        .subscribe(results => {
+          console.log(results);
+          this.results = results;
+        });
+    }
   }
- }
+  getQualityIndicator(score) {
+    let scoreStyle = 'progress-bar '
+    if (score >= 70) {
+      scoreStyle += 'bg-success';
+    } else if (score > 40 && score < 70) {
+      scoreStyle += 'bg-warning';
+    } else {
+      scoreStyle += 'bg-danger';
+    }
+
+    return scoreStyle;
+  }
 }
